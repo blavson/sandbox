d90a11de88b057d5f733d13a14910d3dbef665e4
misha <blava1979@gmail.com>
2019-10-12 00:50:14 +0400

    fixed names

--- src/app/components/form-chalich/form-chalich.component.ts
+++ src/app/components/form-chalich/form-chalich.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild} from '@angular/core';
 import { User } from 'src/app/models/User';
 import { FormsModule } from '@angular/forms';
 import { Applicant } from 'src/app/models/Applicant';
@@ -10,6 +10,7 @@ import { Applicant } from 'src/app/models/Applicant';
 })
 export class FormChalichComponent implements OnInit {
   aplcnt: Applicant;
+  @ViewChild('userForm' , { static : false}) form : any;
   constructor() {
     this.aplcnt = new Applicant();
     this.aplcnt.id = 0;
@@ -18,4 +19,9 @@ export class FormChalichComponent implements OnInit {
   }
 
   ngOnInit() {}
+  
+
+  onSubmit({value, valid } : { value : Applicant, valid : boolean}) {
+    console.log(value.userName);
+  }
 }
